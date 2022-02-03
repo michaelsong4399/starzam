e8bef7983348af159801853b79d9ea407e6a0ec2
Yihong(Michael) Song <46873721+michaelsong4399@users.noreply.github.com>
2022-02-03 01:57:12 -0500

    Update Links

--- Website/pages/logs/index.js
+++ Website/pages/logs/index.js
@@ -1,5 +1,6 @@
 import Navbar from "../../components/frame/Navbar.js";
 import Footer from "../../components/frame/Footer.js";
+import Link from "next/link";
 
 export default function Logs() {
     return (
@@ -11,71 +12,71 @@ export default function Logs() {
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/6"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[6]: Semester Presentation
-                </a>
+                <Link href="/logs/6">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[6]: Semester Presentation
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     February 2, 2022
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/5"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[5]: Algorithm Creation
-                </a>
+                <Link href="/logs/5">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[5]: Algorithm Creation
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     January 6, 2022
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/4"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[4]: Meeting with Dr. Marc Hon
-                </a>
+                <Link href="/logs/4">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[4]: Meeting with Dr. Marc Hon
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     December 20, 2021
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/3"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[3]: Data & Procedure Plan
-                </a>
+                <Link href="/logs/3">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[3]: Data & Procedure Plan
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     November 10, 2021
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/2"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[2]: Assembling Background, Purpose, and Implication
-                </a>
+                <Link href="/logs/2">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[2]: Assembling Background, Purpose, and Implication
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     October 27, 2021
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/1"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[1]: Update Project Plan
-                </a>
+                <Link href="/logs/1">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[1]: Update Project Plan
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     October 26, 2021
                 </div>
             </div>
             <div className="static bg-dark h-auto text-left pt-[3vh] pb-[3vh] px-[10vw] border-t-2 border-solid border-white">
-                <a
-                    href="/logs/0"
-                    className="text-white font-title text-[2rem] hover:text-pink transition-all">
-                    Log[0]: Project Initiation
-                </a>
+                <Link href="/logs/0">
+                    <a className="text-white font-title text-[2rem] hover:text-pink transition-all">
+                        Log[0]: Project Initiation
+                    </a>
+                </Link>
                 <div className="text-white font-title text-[1rem] ">
                     October 18, 2021
                 </div>
